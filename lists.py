d171f0f607cba03be7800a608073ee7f0538bd2d
Muhammadkaj11 <muhdkaj11@gmail.com>
2024-11-29 21:09:50 +0100

    initial commit

---
+++ lists.py
@@ -0,0 +1,5 @@
+list=[]
+list2=[2,4,5,8,90]
+print(list2*3)
+list2.reverse()
+print(list2)
---
+++ main.py
@@ -0,0 +1,12 @@
+ls=[90,60,45,3,1,6,70]
+print("the original list is",ls)
+count=0
+for i in ls:
+    count=count+1
+print("the sum of the list is",count)
+avg=count/len(ls)
+print("the average of the list is",avg)
+ls.sort
+print("the sorted list is",ls)
+print("the largest number i the list is",ls[-1])
+print("the smallest number in the list is",ls[0])
